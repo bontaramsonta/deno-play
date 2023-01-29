858db66e1741a651922dfbc571b5030b1a0d9ba9
bontaramsonta <18cs113@gmail.com>
2023-01-29 19:16:44 +0530

    solving fibonnaci dp

--- main.ts
+++ main.ts
@@ -1,26 +1,30 @@
-export function isPalindrome(x: number): boolean {
-  console.log(
-    x
-      .toString()
-      .substring(0, Math.ceil(x.toString().length / 2))
-      .split("")
-      .reverse()
-      .join("").length
-  );
-  console.log(
-    x.toString().substring(Math.floor(x.toString().length / 2)).length
-  );
-  return (
-    x
-      .toString()
-      .substring(0, Math.ceil(x.toString().length / 2))
-      .split("")
-      .reverse()
-      .join("") == x.toString().substring(Math.floor(x.toString().length / 2))
-  );
+
+const dp = [0,1]
+
+export function fibonnaciNth(n: number): number {
+  dp[n] = (dp[n-1] === undefined ? fibonnaciNth(n-1) : dp[n-1]) + (dp[n-2] === undefined ? fibonnaciNth(n-2) : dp[n-2])
+  return dp[n]
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Palindrome 121=", isPalindrome(121));
+  console.log("fibonnaciNth =", fibonnaciNth(100));
+  // console.log(dp)
 }
+
+Deno.bench({
+  name: "baseline",
+  group: 'group',
+  baseline: true,
+  fn: () => {
+    fibonnaciNth(10)
+  }
+})
+
+Deno.bench({
+  name: "edge case",
+  group: 'group',
+  fn: () => {
+    fibonnaciNth(9)
+  }
+})
