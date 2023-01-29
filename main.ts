949977ea5c73e154ee82e87e86ba56c1e1db026a
bontaramsonta <18cs113@gmail.com>
2023-01-29 22:27:38 +0530

    Solution for longest common subsequence

--- main.ts
+++ main.ts
@@ -1,15 +1,39 @@
 
-const dp = [0,1]
-
-export function fibonnaciNth(n: number): number {
-  dp[n] = (dp[n-1] === undefined ? fibonnaciNth(n-1) : dp[n-1]) + (dp[n-2] === undefined ? fibonnaciNth(n-2) : dp[n-2])
-  return dp[n]
+export function longestCommonSubsequence(a: string, b: string, i: number, j:number, dp: Map<string,number>): number {
+  // one of the strings is empty
+  if( i==0 || j==0 ) {
+    return 0
+  }
+  // already cached max lcs
+  if(dp.has(`${i} ${j}`)) {
+    console.log('[cache hit]')
+    //@ts-ignore checked with .has
+    return dp.get(`${i} ${j}`)
+  }
+  // characters are equal
+  if(a[i-1] == b[j-1]) {
+    const result = 1 + longestCommonSubsequence(a,b,i-1,j-1,dp)
+    dp.set(`${i} ${j}`,result)
+    return result
+  }
+  // characters are not equal
+  else {
+    const result = Math.max(longestCommonSubsequence(a,b,i-1,j,dp),longestCommonSubsequence(a,b,i,j-1,dp))
+    dp.set(`${i} ${j}`,result)
+    return result
+  }
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("fibonnaciNth =", fibonnaciNth(100));
-  // console.log(dp)
+  const a = 'aobcd'
+  const b = 'abed'
+  const dp: Map<string,number> = new Map()
+  // const a = 'abcdavrt'
+  // const b = 'abedagjt'
+  const r = longestCommonSubsequence(a,b,a.length,b.length,dp)
+  console.log(r)
+  console.log(dp)
 }
 
 Deno.bench({
@@ -17,7 +41,11 @@ Deno.bench({
   group: 'group',
   baseline: true,
   fn: () => {
-    fibonnaciNth(10)
+    const dp: Map<string,number> = new Map()
+    const a = 'abcd'
+    const b = 'abed'
+    const r = longestCommonSubsequence(a,b,a.length,b.length,dp)
+    // console.log(r)
   }
 })
 
@@ -25,6 +53,10 @@ Deno.bench({
   name: "edge case",
   group: 'group',
   fn: () => {
-    fibonnaciNth(9)
+    const dp: Map<string,number> = new Map()
+    const a = 'abcdavrtjthsrhsv'
+    const b = 'abedagjthuvaeeyv'
+    const r = longestCommonSubsequence(a,b,a.length,b.length,dp)
+    // console.log(r)
   }
 })
