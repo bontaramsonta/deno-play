1bfd1015d553f81e325e8e0ad20ba6760f1c636b
bontaramsonta <18cs113@gmail.com>
2023-02-12 21:47:17 +0530

    solve: sum of upper & lower triangle in a  matrix

--- main.ts
+++ main.ts
@@ -1,39 +1,31 @@
-export function determinant(
+export function sumOfUpperAndLowerTriangles(
     matrix: number[][],
     n: number,
 ) {
-    if (n == 1) {
-        return matrix[0][0];
-    } else if (n == 2) {
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
-    } else {
-        let result = 0;
-        for (let i = 0; i < n; i++) {
-            // skip if co-eff is zero
-            if (matrix[0][i] !== 0) {
-                // generate cofactor matrix
-                const cofactor = matrix.slice(1).map((row) =>
-                    row.filter((_, index) => index !== i)
-                );
-                if (i % 2 == 0) {
-                    result += matrix[0][i] * determinant(cofactor, n - 1);
-                } else {
-                    result -= matrix[0][i] * determinant(cofactor, n - 1);
-                }
+    let upperTriangleSum = 0;
+    let lowerTriangleSum = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i < j) {
+                upperTriangleSum += matrix[i][j];
+            } else if (i > j) {
+                lowerTriangleSum += matrix[i][j];
+            } else {
+                upperTriangleSum += matrix[i][j];
+                lowerTriangleSum += matrix[i][j];
             }
         }
-        return result;
     }
+    return [upperTriangleSum, lowerTriangleSum];
 }
 
 if (import.meta.main) {
     const matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
+        [6, 5, 4],
+        [1, 2, 5],
+        [7, 9, 7],
     ];
-    const n = 4;
-    const result = determinant(matrix, n);
+    const n = 3;
+    const result = sumOfUpperAndLowerTriangles(matrix, n);
     console.log(result);
 }
