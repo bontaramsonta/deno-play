c6bf88f51987f38fd3ca7daec8402844ec37f93d
bontaramsonta <18cs113@gmail.com>
2023-03-18 13:54:43 +0530

    solve: leetcode same tree

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
-function delayed(label: string, success: boolean, delay: number) {
-    console.log(label, 'called');
-    return new Promise<string>((resolve, reject) =>
-        setTimeout(() => {
-            console.log('[delayed]', label);
-            success ? resolve(label) : reject(`${label} promise reject`);
-        }, delay)
-    );
+import { assertEquals } from 'https://deno.land/std@0.178.0/testing/asserts.ts';
+// Definition for a binary tree node.
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p == null || q == null) return p == q;
+    if (p.val !== q.val) return false;
+    if (isSameTree(p.left, q.left) && isSameTree(p.right, q.right)) return true;
+    else return false;
 }
 
 if (import.meta.main) {
-    const success = delayed('success', true, 3000);
-    const failure = delayed('failure', false, 1000);
-    const failure2 = delayed('failure2', false, 2000);
-    const results = await Promise.allSettled([success, failure, failure2]);
-    console.log(results);
+    const first = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    const second = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    const result = isSameTree(first, second);
+    console.log(result);
+    assertEquals(result, true);
 }
