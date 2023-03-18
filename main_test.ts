1879179d9412f4c92c6dce88d7228e47d652aa68
bontaramsonta <18cs113@gmail.com>
2023-03-18 13:54:55 +0530

    extra

--- main_test.ts
+++ main_test.ts
@@ -1,44 +1,10 @@
 import { assertEquals } from 'https://deno.land/std@0.171.0/testing/asserts.ts';
-import { determinant } from './main.ts';
+// import { determinant } from './main.ts';
 
 Deno.test(function baseTest() {
-    const matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        [13, 14, 15, 16],
-    ];
-    const n = 4;
-    assertEquals(
-        determinant(matrix, n),
-        0,
-    );
+    assertEquals(2 + 2, 4);
 });
 
 Deno.test(function secondTest() {
-    const matrix = [
-        [1, 0, 2, -1],
-        [3, 0, 0, 5],
-        [2, 1, 4, -3],
-        [1, 0, 5, 0],
-    ];
-    const n = 4;
-    assertEquals(
-        determinant(matrix, n),
-        30,
-    );
-});
-
-Deno.test(function thirdTest() {
-    const matrix = [
-        [1, 0, 2, -1],
-        [3, 0, 0, 5],
-        [2, 1, 4, -3],
-        [1, 0, 5, 0],
-    ];
-    const n = 4;
-    assertEquals(
-        determinant(matrix, n),
-        30,
-    );
+    assertEquals(2 + 2, 4);
 });
