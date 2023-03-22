061dae1f6807d749e7d3632b426deb9d51cfd421
bontaramsonta <18cs113@gmail.com>
2023-03-22 23:06:29 +0530

    task 1 : complete readstream pipeto write stream

--- main.ts
+++ main.ts
@@ -1,23 +1,7 @@
-import { readAllSync } from 'https://deno.land/std@0.180.0/streams/read_all.ts';
-import { readLines } from 'https://deno.land/std@0.180.0/io/read_lines.ts';
+// task : read a file and write the contents to another file without loading the whole content
 
 if (import.meta.main) {
-    // way 1
-    const decoder = new TextDecoder();
-    for await (const chunk of Deno.stdin.readable) {
-        const dLine = decoder.decode(chunk);
-        console.log(dLine);
-        console.log('=====');
-    }
-
-    // way2
-    const decoder = new TextDecoder();
-    const buffer = readAllSync(Deno.stdin);
-    const decoded = decoder.decode(buffer);
-    console.log(decoded.length);
-
-    // way 3
-    for await (const line of readLines(Deno.stdin)) {
-        console.log(line);
-    }
+    const readableStream = Deno.openSync('./in.txt').readable;
+    const writableStream = Deno.openSync('./out.txt', { write: true }).writable;
+    await readableStream.pipeTo(writableStream);
 }
