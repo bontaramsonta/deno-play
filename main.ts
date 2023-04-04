c13baa46354281712c1348fd9bb931be451484d0
bontaramsonta <18cs113@gmail.com>
2023-04-04 17:09:27 +0530

    just: appointment dates sorting

--- main.ts
+++ main.ts
@@ -1,68 +1,58 @@
-/** Additional task
- * read space seperated lines numbers from a file
- * and output the sum of prime numbers in the line to another file
- * example
- * in.txt
- * 2 13 17 84 43
- * 34 53 67 56
- * out.txt
- * 75
- * 120
- *
- * DON'T READ THE WHOLE FILE IN MEMORY
- */
-import {
-    TextLineStream,
-    toTransformStream,
-} from 'https://deno.land/std@0.181.0/streams/mod.ts';
-
-export function sumOfPrimes(inPath: string, outPath: string) {
-    const precomputedPrimes = [
-        2,
-        3,
-        5,
-        7,
-        11,
-        13,
-        17,
-        19,
-        23,
-        29,
-        31,
-        37,
-        41,
-        43,
-        47,
-        53,
-        59,
-        61,
-        67,
-        71,
-        73,
-        79,
-        83,
-        89,
-        97,
-    ];
-    const readStream = Deno.openSync(inPath).readable;
-    const writeStream = Deno.openSync(outPath, { write: true }).writable;
-    readStream
-        .pipeThrough(new TextDecoderStream())
-        .pipeThrough(new TextLineStream())
-        .pipeThrough(toTransformStream(async function* (src) {
-            for await (const chunk of src) {
-                yield chunk
-                    .split(' ')
-                    .map((str) => parseInt(str))
-                    .filter((n) => precomputedPrimes.includes(n))
-                    .reduce((acc, val) => acc + val, 0)
-                    .toString() + '\n';
-            }
-        }))
-        .pipeThrough(new TextEncoderStream())
-        .pipeTo(writeStream);
+export function datesGenerator() {
+    let c = 0;
+    const multiplier = (min: number, max: number) =>
+        Math.floor(Math.random() * (max - min + 1)) + min;
+    const dates: [Date | string][] = [];
+    while (c < 100) {
+        const day = 1000 * 60 * 60 * 24;
+        const hour = 1000 * 60 * 60;
+        const date = new Date(
+            Date.now() + (day * multiplier(-5, 30)) +
+                (hour * multiplier(0, 21)),
+        );
+        // console.log(date);
+        if (Math.random() < 0.05) {
+            dates.push(['N/A']);
+        }
+        dates.push([date]);
+        c++;
+    }
+    return dates;
 }
 
 if (import.meta.main) {
-    sumOfPrimes('./in.txt', './out.txt');
+    let dates = datesGenerator();
+    const pastDates: { date: string; offset: number }[] = [];
+    dates = dates.map((data) => {
+        if (typeof data[0] === 'string') {
+            pastDates.push({
+                date: data[0],
+                offset: Number.NEGATIVE_INFINITY,
+            });
+            return null;
+        }
+        const date = data[0];
+        const offset = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
+        if (offset < 0) {
+            pastDates.push({
+                date: date.toLocaleString(),
+                offset: offset,
+            });
+            return null;
+        } else {
+            return {
+                date: date.toLocaleString(),
+                offset: offset,
+            };
+        }
+    }).filter<{ date: string; offset: number }>((a) => !!a);
+    // ascending
+    dates.sort((a, b) => a.offset - b.offset);
+    pastDates.sort((a, b) => a.offset - b.offset);
+    // descending
+    dates.sort((a, b) => b.offset - a.offset);
+    pastDates.sort((a, b) => b.offset - a.offset);
+    // console.log();
+    console.log(dates);
+    console.log(pastDates);
 }
