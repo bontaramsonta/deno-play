1af9d882dd47231db128a9c4f3c155b512ddf2df
bontaramsonta <18cs113@gmail.com>
2023-04-26 18:35:37 +0530

    solve: leetcode Symetric tree

--- main.ts
+++ main.ts
@@ -1,58 +1,30 @@
-export function datesGenerator() {
-    let c = 0;
-    const multiplier = (min: number, max: number) =>
-        Math.floor(Math.random() * (max - min + 1)) + min;
-    const dates: [Date | string][] = [];
-    while (c < 100) {
-        const day = 1000 * 60 * 60 * 24;
-        const hour = 1000 * 60 * 60;
-        const date = new Date(
-            Date.now() + (day * multiplier(-5, 30)) +
-                (hour * multiplier(0, 21)),
-        );
-        // console.log(date);
-        if (Math.random() < 0.05) {
-            dates.push(['N/A']);
-        }
-        dates.push([date]);
-        c++;
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
     }
-    return dates;
 }
 
+function isMirror(left: TreeNode | null, right: TreeNode | null): boolean {
+    if (left === null && right === null) return true;
+    if (left === null || right === null) return false;
+    return left.val === right.val && isMirror(left.left, right.right) &&
+        isMirror(left.right, right.left);
+}
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if (root === null) return true;
+    return isMirror(root.left, root.right);
+}
 if (import.meta.main) {
-    let dates = datesGenerator();
-    const pastDates: { date: string; offset: number }[] = [];
-    dates = dates.map((data) => {
-        if (typeof data[0] === 'string') {
-            pastDates.push({
-                date: data[0],
-                offset: Number.NEGATIVE_INFINITY,
-            });
-            return null;
-        }
-        const date = data[0];
-        const offset = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
-        if (offset < 0) {
-            pastDates.push({
-                date: date.toLocaleString(),
-                offset: offset,
-            });
-            return null;
-        } else {
-            return {
-                date: date.toLocaleString(),
-                offset: offset,
-            };
-        }
-    }).filter<{ date: string; offset: number }>((a) => !!a);
-    // ascending
-    dates.sort((a, b) => a.offset - b.offset);
-    pastDates.sort((a, b) => a.offset - b.offset);
-    // descending
-    dates.sort((a, b) => b.offset - a.offset);
-    pastDates.sort((a, b) => b.offset - a.offset);
-    // console.log();
-    console.log(dates);
-    console.log(pastDates);
+    const Tree = new TreeNode(
+        1,
+        new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+        new TreeNode(2, new TreeNode(4), new TreeNode(3)),
+    );
+    console.log(isSymmetric(Tree));
 }
