4c9b82e40e5c3aa24bd09343855a233b4676ddd9
bontaramsonta <18cs113@gmail.com>
2023-07-04 20:06:01 +0530

    advent of code: day3 can't solve

--- main.ts
+++ main.ts
@@ -1,25 +1,77 @@
-const scoreMap: Map<string, number> = new Map();
-scoreMap.set('A X', 3);
-scoreMap.set('A Y', 4);
-scoreMap.set('A Z', 8);
-scoreMap.set('B X', 1);
-scoreMap.set('B Y', 5);
-scoreMap.set('B Z', 9);
-scoreMap.set('C X', 2);
-scoreMap.set('C Y', 6);
-scoreMap.set('C Z', 7);
+const priorityMap: Record<string, number> = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26,
+    'A': 27,
+    'B': 28,
+    'C': 29,
+    'D': 30,
+    'E': 31,
+    'F': 32,
+    'G': 33,
+    'H': 34,
+    'I': 35,
+    'J': 36,
+    'K': 37,
+    'L': 38,
+    'M': 39,
+    'N': 40,
+    'O': 41,
+    'P': 42,
+    'Q': 43,
+    'R': 44,
+    'S': 45,
+    'T': 46,
+    'U': 47,
+    'V': 48,
+    'W': 49,
+    'X': 50,
+    'Y': 51,
+    'Z': 52,
+};
 
 if (import.meta.main) {
     const contents = Deno.readFileSync('in.txt');
     const decoder = new TextDecoder('utf-8');
     const text = decoder.decode(contents);
-    const lines = text.split('\n').map((line) => line.split(' '));
-    let totalScore = 0;
-    lines.forEach((line) => {
-        const p1Move = line[0];
-        const p2Move = line[1];
-        const score = scoreMap.get(`${p1Move} ${p2Move}`) as number;
-        totalScore += score;
-        console.log(`${p1Move} ${p2Move}`, totalScore);
+    const lines = text.split('\n').map((
+        line,
+    ) => [
+        line.substring(0, line.length / 2),
+        line.substring((line.length / 2) + 1),
+    ]);
+    let totalPriority = 0;
+    lines.forEach((line, index) => {
+        const [firstLine, secondLine] = line;
+        const first = new Set(firstLine);
+        const second = new Set(secondLine);
+        const intersection = new Set([...first].filter((x) => second.has(x)));
+        intersection.forEach((char) => {
+            totalPriority += priorityMap[char];
+            console.log(index, char, priorityMap[char], totalPriority);
+        });
     });
 }
