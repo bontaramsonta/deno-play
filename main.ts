20d5fe9a9c7e0ccca6381cdd149f35a671996aea
bontaramsonta <18cs113@gmail.com>
2023-07-04 10:56:28 +0530

    advent of code: day2 part 1

--- main.ts
+++ main.ts
@@ -1,31 +1,37 @@
-class TreeNode {
-    val: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-        this.val = val === undefined ? 0 : val;
-        this.left = left === undefined ? null : left;
-        this.right = right === undefined ? null : right;
-    }
-}
+const movePoint: Record<string, number> = {
+    X: 1,
+    Y: 2,
+    Z: 3,
+};
 
-function sortedArrayToBST(nums: number[]): TreeNode | null {
-    if (nums.length === 0) {
-        return null;
-    }
-    const mid = Math.floor(nums.length / 2);
-    console.log(mid, nums.slice(0, mid), nums.slice(mid + 1));
-    const node = new TreeNode(
-        nums[mid],
-        sortedArrayToBST(nums.slice(0, mid)),
-        sortedArrayToBST(nums.slice(mid + 1)),
-    );
-    return node;
-}
+const winMap: Map<string, number> = new Map();
+winMap.set('A X', 0);
+winMap.set('A Y', 2);
+winMap.set('A Z', 1);
+winMap.set('B X', 1);
+winMap.set('B Y', 0);
+winMap.set('B Z', 2);
+winMap.set('C X', 2);
+winMap.set('C Y', 1);
+winMap.set('C Z', 0);
 
 if (import.meta.main) {
-    // const arr = [-10, -3, 0, 5, 9];
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const tree = sortedArrayToBST(arr);
-    console.log(tree);
+    const contents = Deno.readFileSync('in.txt');
+    const decoder = new TextDecoder('utf-8');
+    const text = decoder.decode(contents);
+    const lines = text.split('\n').map((line) => line.split(' '));
+    let totalScore = 0;
+    lines.forEach((line) => {
+        const p1Move = line[0];
+        const p2Move = line[1];
+        const p2MovePoint = movePoint[p2Move] as number;
+        totalScore += p2MovePoint;
+        const win = winMap.get(`${p1Move} ${p2Move}`) as number;
+        if (win === 2) {
+            totalScore += 6;
+        } else if (win === 0) {
+            totalScore += 3;
+        }
+        console.log(`${p1Move} ${p2Move}`, win, p2MovePoint, totalScore);
+    });
 }
