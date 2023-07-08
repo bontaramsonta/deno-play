f15ff9335d68082087162f68018799f8d748589d
bontaramsonta <18cs113@gmail.com>
2023-07-08 17:34:16 +0530

    solve: gfg sort array1 according to array2

--- main.ts
+++ main.ts
@@ -1,29 +1,29 @@
-function sortByFrequency(arr: number[]) {
+function sortA1ByA2(arr1: number[], arr2: number[]) {
     const map = new Map<number, number>();
-    const sortedArr: number[] = [];
-    // fill frequency map
-    arr.forEach((element) => {
-        map.set(element, (map.get(element) || 0) + 1);
-    });
-    // sort map by value
-    const sortedMap = new Map([...map.entries()].sort(([k1, v1], [k2, v2]) => {
-        if (v1 === v2) {
-            return k1 - k2;
-        } else {
-            return v2 - v1;
-        }
-    }));
-    // fill sorted array
-    for (const [key, value] of sortedMap.entries()) {
-        for (let i = 0; i < value; i++) {
-            sortedArr.push(key);
-        }
+    const result: number[] = [];
+    // fill map with arr1 frequencies
+    for (const num of arr1) {
+        map.set(num, (map.get(num) || 0) + 1);
     }
-    return sortedArr;
+    // fill result in order of arr2 with arr1 frequencies
+    for (const num2 of arr2) {
+        const freq = map.get(num2) || 0;
+        for (let i = 0; i < freq; i++) {
+            result.push(num2);
+        }
+        map.delete(num2);
+    }
+    // fill result with remaining map keys
+    [...map.entries()].sort((a, b) => a[0] - b[0]).forEach(([key, freq]) => {
+        for (let i = 0; i < freq; i++) {
+            result.push(key);
+        }
+    });
+    return result;
 }
 
 if (import.meta.main) {
-    // const arr = [2, 2, 1, 1, 3, 3, 3, 7, 5, 6, 4];
-    const arr = [5, 5, 4, 6, 4];
-    console.log(sortByFrequency(arr));
+    const arr1 = [45, 15, 23, 8, 5, 12, 26, 444, 888, 151, 12, 23, 45, 15, 56];
+    const arr2 = [15, 888, 444, 5, 8, 12, 23];
+    console.log(sortA1ByA2(arr1, arr2));
 }
