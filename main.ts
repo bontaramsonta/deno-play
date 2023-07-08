0f6bef5819b0efd0e4ce111fd8483a7b45d9939b
bontaramsonta <18cs113@gmail.com>
2023-07-08 16:54:18 +0530

    solve: gfg sort array by frequency of elements

--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
-function printNonRepeated(arr: number[]): number[] {
+function sortByFrequency(arr: number[]) {
     const map = new Map<number, number>();
-    const nonRepeating = [];
+    const sortedArr: number[] = [];
+    // fill frequency map
     arr.forEach((element) => {
         map.set(element, (map.get(element) || 0) + 1);
     });
-    for (const [key, value] of map.entries()) {
-        if (value === 1) {
-            nonRepeating.push(key);
+    // sort map by value
+    const sortedMap = new Map([...map.entries()].sort(([k1, v1], [k2, v2]) => {
+        if (v1 === v2) {
+            return k1 - k2;
+        } else {
+            return v2 - v1;
+        }
+    }));
+    // fill sorted array
+    for (const [key, value] of sortedMap.entries()) {
+        for (let i = 0; i < value; i++) {
+            sortedArr.push(key);
         }
     }
-    return nonRepeating;
+    return sortedArr;
 }
 
 if (import.meta.main) {
-    const arr = [1, 1, 2, 2, 3, 3, 7, 5, 6, 4];
-    console.log(printNonRepeated(arr));
+    // const arr = [2, 2, 1, 1, 3, 3, 3, 7, 5, 6, 4];
+    const arr = [5, 5, 4, 6, 4];
+    console.log(sortByFrequency(arr));
 }
