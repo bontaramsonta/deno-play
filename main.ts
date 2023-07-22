5d5810b8fbe61c06e9bda2c918f8f0310203d95b
bontaramsonta <18cs113@gmail.com>
2023-07-22 23:19:42 +0530

    can't solve: gfg Check if array is sorted and rotated

--- main.ts
+++ main.ts
@@ -1,29 +1,49 @@
-function minJumps(arr: number[], n: number) {
-    let jumps = 0;
-    let index = 0;
-    while (index < n) {
-        jumps++;
-        // console.log('On jump', jumps, 'index', index, 'with value', arr[index]);
-        let minCostFromEnd = Number.POSITIVE_INFINITY;
-        let minCostFromEndIndex = -1;
-        let offset = arr[index];
-        while (offset > 0) {
-            const costFromEnd = n - (index + offset);
-            if (costFromEnd < minCostFromEnd) {
-                minCostFromEnd = costFromEnd;
-                minCostFromEndIndex = index + offset;
+function checkRotatedAndSorted(arr: number[], num: number) {
+    // it is true for 1 or 2 elements arrays
+    if (num === 1 || num === 2) return true;
+    // compare first two elements to determine the trend
+    // if first two elements are equal, compare the second and third elements
+    let trend: 'asc' | 'desc' = arr[0] === arr[1]
+        ? arr[1] < arr[2] ? 'asc' : 'desc'
+        : arr[0] < arr[1]
+        ? 'asc'
+        : 'desc';
+    console.log('trend', trend);
+    // if the trend changes more than once, return false
+    let trendChangeCount = 0;
+    for (let i = 1; i < num; i++) {
+        if (trend === 'asc') {
+            if (i === num - 1 && arr[i] > arr[0]) {
+                console.log('change trend at', i, 'to desc');
+                trendChangeCount++;
+                trend = 'desc';
             }
-            offset--;
+            if (arr[i] > arr[i + 1]) {
+                console.log('change trend at', i, 'to desc');
+                trendChangeCount++;
+                trend = 'desc';
+            }
+        } else {
+            if (i === num - 1 && arr[i] < arr[0]) {
+                console.log('change trend at', i, 'to asc');
+                trendChangeCount++;
+                trend = 'asc';
+            }
+            if (arr[i] < arr[i + 1]) {
+                console.log('change trend at', i, 'to asc');
+                trendChangeCount++;
+                trend = 'asc';
+            }
         }
-        index = minCostFromEndIndex;
     }
-    return jumps;
+    return trendChangeCount === 2;
 }
 if (import.meta.main) {
-    const inputs = (await Deno.readTextFile('in.txt')).split('\n').map(
-        (lines) => lines.split(' ').map(Number),
-    );
-    inputs.forEach((input) => {
-        console.log(minJumps(input, input.length));
-    });
+    // const inputs = (await Deno.readTextFile('in.txt')).split('\n').map(
+    //     (lines) => lines.split(' ').map(Number),
+    // );
+    // inputs.forEach((input) => {
+    //     console.log(checkRotatedAndSorted(input, input.length));
+    // });
+    console.log(checkRotatedAndSorted([4, 1, 2, 3], 4));
 }
