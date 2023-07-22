98da65601fbc96a1bacb129cbed2eb014bdc69a2
bontaramsonta <18cs113@gmail.com>
2023-07-22 20:19:57 +0530

    kadane's algo

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
-function LongestRepeatingSubsequence(str: string): number {
-    const n = str.length;
-    const dp: number[][] = Array.from(
-        { length: n + 1 },
-        () => Array(n + 1).fill(0),
-    );
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str[i - 1] === str[j - 1] && i !== j) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
+//Function to find the sum of contiguous subarray with maximum sum.
+function maxSubarraySum(arr: number[], N: number) {
+    let max = Number.NEGATIVE_INFINITY;
+    let sum = 0;
+    for (let i = 0; i < N; i++) {
+        sum += arr[i];
+        if (sum > max) {
+            max = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
         }
     }
-    console.log(dp);
-    return dp[n][n];
+    return max;
 }
 if (import.meta.main) {
-    const s = 'axxxy';
-    console.log(LongestRepeatingSubsequence(s));
+    // const s = [1, 2, 3, -2, 5];
+    const s = [-47, 43, 94, -94, -93, -59, 31, -86];
+    console.log(maxSubarraySum(s, s.length));
 }
