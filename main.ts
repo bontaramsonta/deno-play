5c84ee214f7ca8c004f3a49e747a4136db1ad9a7
bontaramsonta <18cs113@gmail.com>
2023-09-10 20:52:33 +0530

    solve: GFG coin change

--- main.ts
+++ main.ts
@@ -1,75 +1,21 @@
-import inputJson from './input.json' assert { type: 'json' };
-import lodash from 'npm:lodash';
-const fields = ['Groupings', 'Type5', 'Type4', 'Type3'] as const;
-type Field = typeof fields[number];
-type Input = {
-    id: number;
-    year: number;
-    Type: string;
-    Groupings: string;
-    Type5: string;
-    Type4: string;
-    Type3: string;
-    Type2: string;
-    Region: string;
-    Currency: string;
-    month: string;
-    value: number;
-    NewValue: string;
-};
+import { assertEquals } from 'https://deno.land/std@0.201.0/assert/mod.ts';
 
-type Output = {
-    sno: string;
-    id: string;
-    category: string;
-    value: number;
-    children: Output[];
-};
-
-function groupDataBasedonFields(
-    inputJson: Input[],
-    fields: Field[],
-    key = 'main',
-    _parentId = 'main',
-    _currentField = 0,
-): Output {
-    const field = fields[_currentField];
-    const groupedData = lodash.groupBy(inputJson, field);
-    const result = [];
-    for (let i = 0; i < Object.keys(groupedData).length; i++) {
-        const key = Object.keys(groupedData)[i];
-        const value = groupedData[key];
-        const output: Output = {
-            sno: `${_parentId}.${key}.${i + 1}`,
-            id: key,
-            category: field,
-            value: lodash.sumBy(value, 'value'),
-            children: [],
-        };
-        if (_currentField < fields.length - 1) {
-            output.children.push(
-                groupDataBasedonFields(
-                    value,
-                    fields,
-                    key,
-                    `${_parentId}.${key}.${i + 1}`,
-                    _currentField + 1,
-                ),
-            );
-        }
-        result.push(output);
+function coinChange(coins: number[], sum: number) {
+    if (sum === 0) {
+        return 1;
+    } else if (sum < 0 || coins.length === 0) {
+        return 0;
     }
-    return {
-        sno: _parentId,
-        id: key,
-        category: field,
-        value: lodash.sumBy(result, 'value'),
-        children: result,
-    };
+    let ways = 0;
+    // include first coin
+    ways += coinChange(coins, sum - coins[0]);
+    // remove first coin
+    ways += coinChange(coins.slice(1), sum);
+    return ways;
 }
 
 if (import.meta.main) {
-    const result = groupDataBasedonFields(inputJson, fields);
-    console.log(JSON.stringify(result, null, 2));
-    // console.log(JSON.stringify(groupedData, null, 2));
+    const result = coinChange([1, 2, 3], 4);
+    console.log(result);
+    assertEquals(result, 4);
 }
