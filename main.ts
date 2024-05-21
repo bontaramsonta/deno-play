9b6fcba1d847f130cc564cf94ccf50fddf3612c6
souravdhar <bontaramsonta@gmail.com>
2024-05-21 10:21:44 +0530

    deno by example: Deno watch.fs

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-console.log('main file');
+import { debounce } from 'jsr:@std/async/debounce';
+
+console.log('[%s] Started', 'main');
+
+const log = debounce((event: Deno.FsEvent) => {
+  console.log('[%s] %s', event.kind, event.paths[0]);
+}, 200);
+
+const shouldReadDirectory = await Deno.permissions.request({
+  name: 'read',
+  path: './',
+});
+
+if (!shouldReadDirectory) {
+  throw new Error('Permission denied');
+}
+
+const watcher = Deno.watchFs('./');
+
+for await (const event of watcher) {
+  log(event);
+}
