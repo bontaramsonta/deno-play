9025faea33c490696bee16ee39bcd5e1e5b2df00
souravdhar <bontaramsonta@gmail.com>
2024-05-26 22:35:28 +0530

    solve: gfg potd minCostToMakeStringsIdentical

--- main.ts
+++ main.ts
@@ -1,25 +1,35 @@
-const a = [8, 3, 1, 5, -6, 6, 2, 2];
-const sum = 4;
-
-function longestSubsequenceWithGivenSum(arr: number[], sum: number) {
-  const hash = new Map<number, number>();
-  let maxLen = 0;
-  let currentSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    currentSum += arr[i];
-    const v = hash.get(currentSum - sum);
-    if (v === undefined) {
-      if (!hash.has(currentSum)) {
-        hash.set(currentSum, i);
-      }
-    } else {
-      const len = i - v;
-      if (len > maxLen) {
-        maxLen = len;
+function longestCommonSubsequence(a: string, b: string) {
+  const dp = new Array(a.length + 1).fill(0).map(() =>
+    new Array(b.length + 1).fill(0)
+  );
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
   }
-  return maxLen;
+  console.log(dp);
+  return dp[a.length][b.length];
 }
-const result = longestSubsequenceWithGivenSum(a, sum);
-console.log(result);
+
+function minCostToMakeStringsIdentical(
+  a: string,
+  b: string,
+  costX: number,
+  costY: number,
+) {
+  const lcs = longestCommonSubsequence(a, b);
+  const diffA = a.length - lcs;
+  const diffB = b.length - lcs;
+  return diffA * costX + diffB * costY;
+}
+if (import.meta.main) {
+  const str1 = 'abcd';
+  const str2 = 'acdb';
+  const costX = 10;
+  const costY = 20;
+  console.log(minCostToMakeStringsIdentical(str1, str2, costX, costY));
+}
