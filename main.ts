fea483806cdfc9d26e0ca8ae1cd5a0467a2fd8ca
souravdhar <bontaramsonta@gmail.com>
2024-08-29 20:26:04 +0530

    linkedlist DST

--- main.ts
+++ main.ts
@@ -1,50 +1,77 @@
-class Node {
-  data: number;
-  next: Node | null;
-
-  constructor(data: number) {
-    this.data = data;
-    this.next = null;
-  }
-}
-const nodeMap = new Map<Node, number>();
-
-function countNodesInLoop(head: Node) {
-  let current: Node | null = head;
-  let index: number = 0;
-  while (current) {
-    console.log(current.data, '->');
-    if (nodeMap.has(current)) {
-      return index - nodeMap.get(current)!;
+class LinkedList {
+  private Node: new (data: number) => {
+    data: number;
+    next: InstanceType<LinkedList['Node']> | null;
+  } = class {
+    data: number;
+    next: InstanceType<LinkedList['Node']> | null;
+    constructor(data: number) {
+      this.data = data;
+      this.next = null;
     }
-    nodeMap.set(current, index);
-    current = current.next;
-    index++;
+  };
+
+  private head: InstanceType<LinkedList['Node']> | null;
+  private current: InstanceType<LinkedList['Node']> | null;
+
+  constructor() {
+    this.head = null;
+    this.current = null;
   }
-  return 0;
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data, '->');
+      current = current.next;
+    }
+    console.log('null');
+  }
+  from(arr: number[]) {
+    if (arr.length === 0) return;
+    this.head = new this.Node(arr[0]);
+    let current = this.head;
+    let i = 1;
+    while (i < arr.length) {
+      current.next = new this.Node(arr[i]);
+      current = current.next;
+      i++;
+    }
+    this.current = this.head;
+  }
+  *[Symbol.iterator]() {
+    let current = this.head;
+    while (current) {
+      yield current.data;
+      current = current.next;
+    }
+  }
+  reset() {
+    this.current = this.head;
+  }
+  add(data: number) {
+    if (this.current) {
+      this.current.next = new this.Node(data);
+      this.current = this.current.next;
+    } else {
+      this.head = new this.Node(data);
+      this.current = this.head;
+    }
+  }
+  get(): number | null {
+    return this.current ? this.current.data : null;
+  }
 }
 
 if (import.meta.main) {
-  // LinkedList: 25->14->19->33->10->21->39->90->58->45->33
-  const head = new Node(25);
-  const second = new Node(14);
-  const third = new Node(19);
-  const fourth = new Node(33); // also eleventh
-  const fifth = new Node(10);
-  const sixth = new Node(21);
-  const seventh = new Node(39);
-  const eighth = new Node(90);
-  const ninth = new Node(58);
-  const tenth = new Node(45);
-  head.next = second;
-  second.next = third;
-  third.next = fourth;
-  fourth.next = fifth;
-  fifth.next = sixth;
-  sixth.next = seventh;
-  seventh.next = eighth;
-  eighth.next = ninth;
-  ninth.next = tenth;
-  tenth.next = fourth;
-  console.log(countNodesInLoop(head));
+  const list = new LinkedList();
+  list.from([1, 2, 3, 4, 5, 6]);
+  list.print();
+  list.reset();
+  for (const data of list) {
+    console.log(data);
+  }
+  list.reset();
+  list.print();
+  list.add(7);
+  list.print();
 }
