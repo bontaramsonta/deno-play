ffa790fd22373f2e308195f420389780117c740d
souravdhar <bontaramsonta@gmail.com>
2025-01-15 21:11:42 +0530

    this has to be signed

--- main.ts
+++ main.ts
@@ -1,210 +1,29 @@
-import { assert, assertEquals } from 'jsr:@std/assert';
+import { Buffer } from 'node:buffer';
 
-class MinHeap {
-    private heap: number[];
-
-    constructor(arr: number[] = []) {
-        this.heap = arr;
-        if (arr.length < 2) {
-            return this;
-        }
-        const parentOfLastNode = this.parent(this.heap.length - 1);
-        for (let i = parentOfLastNode; i >= 0; i--) {
-            this.minHeapify(i);
-        }
-        return this;
+function shiftingLetters(s: string, shifts: number[][]): string {
+    const buffer = Buffer.from(s, 'ascii');
+    const prefixShiftArray: number[] = new Array(s.length + 1).fill(0);
+    for (const [startIdx, endIdx, direction] of shifts) {
+        const val = direction === 0 ? -1 : 1;
+        const l = startIdx;
+        const r = endIdx + 1;
+        prefixShiftArray[l] += -val;
+        prefixShiftArray[r] += val;
     }
-
-    static from(arr: number[]) {
-        return new MinHeap(arr);
+    let diff = prefixShiftArray.at(-1)!;
+    console.log(prefixShiftArray.join(','));
+    for (let i = prefixShiftArray.length - 2; i >= 0; i--) {
+        console.log(buffer[i], diff);
+        diff = diff < 0 ? (diff % 26) + 26 : diff;
+        buffer[i] = ((buffer[i] - 97 + diff) % 26) + 97;
+        diff += prefixShiftArray[i];
     }
-
-    getSize() {
-        return this.heap.length;
-    }
-
-    getHeight() {
-        return Math.floor(Math.log2(this.heap.length));
-    }
-
-    print() {
-        if (this.heap.length === 0) {
-            console.log('Heap is empty.');
-            return;
-        }
-        return this.heap.join(', ');
-    }
-
-    checkMinHeapProperty() {
-        const size = this.heap.length;
-        if (size < 2) {
-            return true;
-        }
-        for (let pos = size - 2; pos > 0; pos--) {
-            const parentPos = this.parent(pos);
-            if (this.heap[parentPos] > this.heap[pos]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    insert(num: number) {
-        this.heap.push(num);
-        this.bubbleUp(this.heap.length - 1);
-    }
-
-    extractMin() {
-        this.swap(0, this.heap.length - 1);
-        const min = this.heap.pop();
-        this.minHeapify(0);
-        return min;
-    }
-
-    decrease(index: number, newValue: number) {
-        const currentValue = this.heap[index];
-        if (!currentValue) {
-            throw new Error('index out of bounds');
-        }
-        if (newValue > currentValue) {
-            throw new Error('value passed is greater than current value');
-        }
-        this.heap[index] = newValue;
-        this.bubbleUp(index);
-    }
-
-    // time Complexity: O(logn)
-    private minHeapify(pos: number) {
-        const left = this.left(pos);
-        const right = this.right(pos);
-        let smallest = pos;
-        if (
-            left < this.heap.length && this.heap[left] < this.heap[smallest]
-        ) {
-            smallest = left;
-        }
-        if (
-            right < this.heap.length &&
-            this.heap[right] < this.heap[smallest]
-        ) {
-            smallest = right;
-        }
-        console.log(
-            'min heapify',
-            { left, right, pos, smallest, heap: this.heap },
-        );
-        if (smallest !== pos) {
-            this.swap(smallest, pos);
-            this.minHeapify(smallest);
-        }
-    }
-
-    delete(pos: number) {
-        if (!this.getSize()) {
-            return;
-        }
-        this.decrease(pos, Number.NEGATIVE_INFINITY);
-        this.extractMin();
-    }
-
-    private bubbleUp(pos: number) {
-        if (pos === 0) {
-            return;
-        }
-        const parent = this.parent(pos);
-        if (this.heap[pos] >= this.heap[parent]) {
-            return;
-        }
-        this.swap(pos, parent);
-        this.bubbleUp(parent);
-    }
-
-    private swap(posA: number, posB: number) {
-        const temp = this.heap[posA];
-        this.heap[posA] = this.heap[posB];
-        this.heap[posB] = temp;
-    }
-
-    private parent(pos: number) {
-        return Math.floor((pos - 1) / 2);
-    }
-
-    private left(pos: number) {
-        return 2 * pos + 1;
-    }
-
-    private right(pos: number) {
-        return 2 * pos + 2;
-    }
+    return buffer.toString();
 }
 
-Deno.test('construct min heap by inserting one by one', () => {
-    const heap = new MinHeap();
-    const arr = [29, 5, 10, 15, 12, 21];
-    console.log('[insert] arr of elements to insert', arr);
-    for (const e of arr) {
-        heap.insert(e);
-    }
-    console.log('[insert] heap after inserts', heap);
-    assertEquals(6, heap.getSize(), 'heap size not matching');
-    assert(
-        heap.checkMinHeapProperty(),
-        `min heap property violated \n${heap.print()}`,
-    );
-});
-
-Deno.test('construct min heap from arr directly', async (t) => {
-    const arr = [29, 5, 10, 15, 12, 21];
-    console.log('[from] arr to create heap from', arr);
-
-    await t.step('using new', () => {
-        const heap = new MinHeap(arr);
-        console.log('[from] heap created directly from arr', heap);
-        assertEquals(6, heap.getSize(), 'heap size not matching');
-        assert(
-            heap.checkMinHeapProperty(),
-            `min heap property violated \n${heap.print()}`,
-        );
-    });
-
-    await t.step('using from', () => {
-        const heap = MinHeap.from(arr);
-        console.log('[from] heap created directly from arr', heap);
-        assertEquals(6, heap.getSize(), 'heap size not matching');
-        assert(
-            heap.checkMinHeapProperty(),
-            `min heap property violated \n${heap.print()}`,
-        );
-    });
-});
-
-Deno.test('extract min', () => {
-    const arr = [29, 5, 10, 15, 12, 21];
-    const heap = MinHeap.from(arr);
-    assertEquals(heap.getSize(), 6);
-    const min = heap.extractMin();
-    assertEquals(min, 5, 'extracted min value is not correct');
-    assertEquals(heap.getSize(), 5);
-    assert(heap.checkMinHeapProperty(), heap.print());
-});
-
-Deno.test('decrement key', () => {
-    const heap = MinHeap.from([29, 5, 10, 15, 12, 21]);
-    heap.decrease(2, 3);
-    assert(heap.checkMinHeapProperty());
-    assertEquals(heap.extractMin(), 3);
-});
-
-Deno.test('delete key', () => {
-    const heap = MinHeap.from([29, 5, 10, 15, 12, 21]);
-    heap.delete(2);
-    assert(heap.checkMinHeapProperty());
-    assertEquals(heap.getSize(), 5);
-});
-
 if (import.meta.main) {
-    const arr = [29, 5, 10, 15, 12, 21];
-    console.log('[from] arr to create heap from', arr);
-    const heap = new MinHeap(arr);
-    console.log('[from] heap created directly from arr', heap);
+    const inputStr = 'abc'; // a(0)c(1)e(2)
+    const inputShifts = [[0, 1, 0], [1, 2, 1], [0, 2, 1]];
+    const result = shiftingLetters(inputStr, inputShifts);
+    console.log(result);
 }
